a202f919e69c779df283d633c71b34e9156146e8
cdincer <dincercan@gmail.com>
2022-09-22 19:59:53 +0300

    propTypes demonstration

--- frontend/src/components/CourseList.js
+++ frontend/src/components/CourseList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function CourseList(props) {
   return (
@@ -24,5 +25,16 @@ function CourseList(props) {
     </table>
   );
 }
+//runs on only Development Mode not in production
+CourseList.propTypes = {
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequred,
+      title: PropTypes.string.isRequired,
+      authorId: PropTypes.number.isRequired,
+      category: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default CourseList;
