b1ae41b0275d77f763414fe88e5c64c4ab678cc4
cdincer <dincercan@gmail.com>
2022-10-20 19:26:05 +0300

    React - Flux CRUD

--- frontend/src/components/CoursesPage.js
+++ frontend/src/components/CoursesPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import CourseList from "./CourseList";
 import { Link } from "react-router-dom";
 import courseStore from "../stores/courseStore";
-import { loadCourses } from "../actions/courseActions";
+import { loadCourses, deleteCourse } from "../actions/courseActions";
 
 function CoursesPage() {
   const [courses, setCourses] = useState(courseStore.getCourses());
@@ -23,7 +23,7 @@ function CoursesPage() {
         Add Course
       </Link>
       <p></p>
-      <CourseList courses={courses} />
+      <CourseList courses={courses} deleteCourse={deleteCourse} />
     </>
   );
 }
