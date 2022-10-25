e440685492cbeae199d1e45712e83706eb385586
cdincer <dincercan@gmail.com>
2022-10-25 18:55:29 +0300

    New Beginning

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { render } from "react-dom";
-import App from "./components/App";
-import { BrowserRouter as Router } from "react-router-dom";
 
-render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
+function Hi() {
+  return <p>Hi.</p>;
+}
+
+render(<Hi />, document.getElementById("app"));
