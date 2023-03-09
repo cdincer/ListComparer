715777c64ff214f81fadd00d3ee93aac8d21b88b
cdincer <dincercan@gmail.com>
2023-03-09 19:26:58 +0100

    Code Clean up and Sorting first version

--- frontend/src/components/Wishlist/GOGWishList.js
+++ frontend/src/components/Wishlist/GOGWishList.js
@@ -1,54 +1,72 @@
-import {  useState } from "react";
+import { useState } from "react";
 import Container from 'react-bootstrap/Container';
 
 export default function GOGWishList() {
- const [ GOGWishListGames, setGOGWishlist ] = useState([]);
- const [SumOfWishListedGOGGames,setSumOfWishListedGOGGames] = useState([]);
+  const [GOGWishListGames, setGOGWishlist] = useState([]);
+  const [SumOfWishListedGOGGames, setSumOfWishListedGOGGames] = useState([]);
 
- 
-const handleClick = async (event) => {
-  event.preventDefault();
 
-  var optCarrier =  require('../../Addresses.json');
-  var GOGWishListAddressVar="";
+  const handleClick = async (event) => {
+    event.preventDefault();
 
-  for (var i = 0; i < optCarrier.length; i++)
-  {
+    var optCarrier = require('../../Addresses.json');
+    var GOGWishListAddressVar = "";
+
+    for (var i = 0; i < optCarrier.length; i++) {
       var obj = optCarrier[i];
-      if(obj.Name === 'GOGWishListAddress')
-      {
-        GOGWishListAddressVar =obj.Value;
+      if (obj.Name === 'GOGWishListAddress') {
+        GOGWishListAddressVar = obj.Value;
         break;
       }
+    }
+
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ profileAddress: GOGWishListAddressVar })
+    };
+    const response = await fetch(
+      'https://localhost:7181/GOG', requestOptions);
+    const data = await response.json();
+    const newArray = SortIt(data);
+    setGOGWishlist(newArray);
+    let sum = SumOfWishList(data);
+    setSumOfWishListedGOGGames(sum.toFixed(2));
+    console.log('TO-DO:Add Performance Counter');
+
+  };
+
+  //TO-DO:Will be implementing quick sort and slimming down the parseFloat bloat.
+  function SortIt(origArray) {
+    for (let i = 0; i < origArray.length; i++) {
+      for (let y = 0; y < origArray.length; y++) {
+        if (parseFloat(parseFloat(origArray[i].Price).toFixed(2)) < parseFloat(parseFloat(origArray[y].Price).toFixed(2))) {
+          var temp = origArray[y];
+          origArray[y] = origArray[i];
+          origArray[i] = temp;
+        }
+      }
+    }
+    return origArray;
   }
 
-
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ profileAddress: GOGWishListAddressVar })
-};
-    const response = await fetch(
-    'https://localhost:7181/GOG',requestOptions);
-     const data = await response.json();
-     setGOGWishlist(data);
-     let sum =0;
-     for (let i = 0; i < data.length; i++) {
-      let PriceVariable =0;
-      if (Number.isInteger(parseInt(data[i].Price) )) {
-        PriceVariable = parseFloat(data[i].Price.replace(',','.').replace(' ',''))
+  function SumOfWishList(WTBS) {
+    let sum = 0;
+    for (let i = 0; i < WTBS.length; i++) {
+      let PriceVariable = 0;
+      if (Number.isInteger(parseInt(WTBS[i].Price))) {
+        PriceVariable = parseFloat(WTBS[i].Price.replace(',', '.').replace(' ', ''))
       }
       sum = sum + PriceVariable;
-      setSumOfWishListedGOGGames(sum.toFixed(2));
+    }
+    return sum;
   }
-     console.log('TO-DO:Add Performance Counter');
 
-};
- 
- return (
-  <Container className="container">
-  <div className="col-sm">
-  <table className="table table-bordered table-striped">
+  return (
+    <Container className="container">
+      <div className="col-sm">
+        <table className="table table-bordered table-striped">
           <thead>
             <tr>
               <th>Title</th>
@@ -56,28 +74,28 @@ const handleClick = async (event) => {
             </tr>
           </thead>
           <tbody>
-       
-         {
-         GOGWishListGames.map( (game,key) =>
-            <tr key={key}>
-              <td>
-               {game.Name}
-              </td>
-              <td>
-              {game.Price}
-              </td>
+
+            {
+              GOGWishListGames.map((game, key) =>
+                <tr key={key}>
+                  <td>
+                    {game.Name}
+                  </td>
+                  <td>
+                    {game.Price}
+                  </td>
+                </tr>
+              )
+            }
+            <tr><td>Total of All Wishlisted GOG Games</td>
+              <td>{SumOfWishListedGOGGames}</td>
             </tr>
-         )
-}
-        <tr><td>Total of All Wishlisted GOG Games</td>
-        <td>{SumOfWishListedGOGGames}</td>
-        </tr>
           </tbody>
         </table>
         <form onSubmit={handleClick}>
-    <button type="submit" className="btn">Get GOG Wish List</button>    
-    </form>
-  </div>
-</Container>
- );
+          <button type="submit" className="btn">Get GOG Wish List</button>
+        </form>
+      </div>
+    </Container>
+  );
 }
